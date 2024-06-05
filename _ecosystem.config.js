73e61c610dc3dcaa786d0ab42586c58a799ac7d1
Tu Van <tu.van@gosell.vn>
2024-06-06 05:42:13 +0700

    tool fighter

--- _ecosystem.config.js
+++ _ecosystem.config.js
@@ -96,5 +96,13 @@ module.exports = {
       watch: false,
       max_memory_restart: "200M",
     },
+    {
+      name: "CatBoots",
+      script: "./cat/index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "200M",
+    },
   ],
 };
---
+++ pixel/fighter.js
@@ -0,0 +1,27 @@
+var mdown = new MouseEvent("pointerdown", { view: null, bubbles: true, cancelable: true });
+var autoclick = function () {
+  var clickableArea = document.getElementsByClassName("clickableArea");
+  if (clickableArea) {
+    clickableArea[0]?.dispatchEvent(mdown);
+    clickableArea[0]?.click();
+  }
+};
+var autoclickBtn = function () {
+  var ele = document.querySelector('button[class*="_button_"][class*="_purple_"][class*="_textUppercase_"]');
+  if (ele) {
+    console.log(".");
+    ele?.dispatchEvent(mdown);
+    ele?.click();
+  }
+};
+
+function start() {
+  if (window.auto) clearInterval(window.auto);
+  if (window.click) clearInterval(window.click);
+  window.auto = setInterval(autoclick, 100);
+  window.click = setInterval(autoclickBtn, 50);
+}
+
+function stop() {
+  if (window.click) clearInterval(window.click);
+}
